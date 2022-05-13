cc9c710d4010f704ee529082bdd71a846a7ffba1
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-13 14:39:55 +0530

    removed: unnecesary files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <div className="bg-slate-500 ">
+      Hello
+    </div>
+  )
+}
 
 export default IndexPage
